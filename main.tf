a113a7572f68c2eafd433836f5c3c690d91deee4
chintu <vineethcmi5@gmail.com>
2024-09-24 14:13:52 +0530

    Deploying a Cluster of Web Servers

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
+  ami                    = "ami-0e86e20dae9224db8"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
   user_data              = <<-EOF
@@ -31,4 +31,44 @@ variable "server_port" {
 output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "The public IP address of the web server"
+}
+
+# Deploying a Cluster of Web Servers
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0e86e20dae9224db8"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  user_data       = <<-EOF
+ #!/bin/bash
+ echo "Hello, World" > index.html
+ nohup busybox httpd -f -p ${var.server_port} &
+ EOF
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+  launch_configuration = aws_launch_configuration.example.name
+  min_size             = 2
+  max_size             = 10
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
