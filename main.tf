1b8b60b96607d5a1e08b747e9a0a75abbcc0fce3
chintu <vineethcmi5@gmail.com>
2024-09-24 13:56:46 +0530

    Deploy a single web server

--- main.tf
+++ main.tf
@@ -1,12 +1,23 @@
-provider "aws" {
- region = "us-east-1"
- }
-
- resource "aws_instance" "example" {
- ami          = "ami-0e86e20dae9224db8"
- instance_type = "t2.micro"
+resource "aws_instance" "example" {
+  ami                    = "ami-0fb653ca2d3203ac1"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data              = <<-EOF
+ #!/bin/bash
+ echo "Hello, World" > index.html
+ nohup busybox httpd -f -p 8080 &
+ EOF
   tags = {
- Name = "terraform-example"
- }
- }
+    Name = "terraform-example"
+  }
+}
 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
